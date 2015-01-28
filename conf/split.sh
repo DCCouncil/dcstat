2b9ad30bbad98a06e8fc22f9c1a5665313023f53
vzvenyach <vdavez@gmail.com>
2015-01-28 07:05:11 -0500

    fixed the s3 location for the split script

--- conf/split.sh
+++ conf/split.sh
@@ -11,8 +11,8 @@ zip dcstat_20.zip compress/*.pdf
 python lib/pdf_to_json.py
 zip dcstat_20_json.zip json/*.json
 aws s3 cp data/metadata.json s3://dcstat --acl public-read --profile council
-aws s3 cp dcstat_20.zip s3://dcstat/public --acl public-read --profile council
-aws s3 cp dcstat_20_json.zip s3://dcstat/public --acl public-read --profile council
+aws s3 cp dcstat_20.zip s3://dcstat/public/dcstat_20.zip --acl public-read --profile council
+aws s3 cp dcstat_20_json.zip s3://dcstat/public/dcstat_20_json.zip --acl public-read --profile council
 aws s3 cp dcstat_20.pdf s3://dcstat/public --acl public-read --profile council
 rm compress/*.pdf
 rm tmp/*.pdf
